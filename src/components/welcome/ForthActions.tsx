603b6035ea8aa69f8199ccd56ebeace7ee4d7480
YongTing <jiangyongting0810@gmail.com>
2023-07-30 13:32:27 +0800

    创建 FloatButton 组件

--- src/components/welcome/ForthActions.tsx
+++ src/components/welcome/ForthActions.tsx
@@ -5,7 +5,7 @@ import s from './welcome.module.scss'
 export const ForthActions:FunctionalComponent=()=>{
   return <div class={s.actions}>
     <RouterLink class={s.fake} to="/start">跳过</RouterLink>
-    <RouterLink class={s.next} to="/welcome/2">完成</RouterLink>
+    <RouterLink class={s.next} to="/start">完成</RouterLink>
     <RouterLink class={s.fake} to="/start">跳过</RouterLink>
   </div>
 }
