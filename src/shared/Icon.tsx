95e86b06d722f174598dd0ed1dbbc6fba5fcf658
YongTing <jiangyongting0810@gmail.com>
2023-08-02 06:48:47 +0800

    创建 Overlay 组件

--- src/shared/Icon.tsx
+++ src/shared/Icon.tsx
@@ -1,18 +1,21 @@
 import { defineComponent, PropType } from 'vue';
 import s from './Icon.module.scss';
 
-export type IconName = 'add'| 'blueberries' | 'easyBooks' | 'intelliBooks' |'onTheGo'| 'worryLess' | 'record' | 'menu'
+export type IconName = 'add'| 'blueberries' | 'easyBooks' | 'intelliBooks' |'onTheGo'| 'worryLess' | 'record' | 'menu'| 'charts' | 'notify' | 'export'
 
 export const Icon = defineComponent({
   props:{
     name:{
       type:String as PropType<IconName>,
       required:true,
+    },
+    onClick:{
+      type:Function as PropType<(e:MouseEvent) => void>
     }
   },
   setup: (props, context) => {
      return () => (
-      <svg class={s.icon}>
+      <svg class={s.icon} onClick={props.onClick}>
         <use xlinkHref={"#"+props.name}></use>
       </svg>
      )
--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,21 +1,27 @@
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { Button } from '../shared/Button';
 import { Center } from '../shared/Center';
 import { FloatButton } from '../shared/FloatButton';
 import { Icon } from '../shared/Icon';
 import { Navbar } from '../shared/Navbar';
+import { Overlay } from './Overlay';
 import s from './StartPage.module.scss'
 export const StartPage = defineComponent({
   setup: (props, context) => {
     const onClick = ()=>{
       console.log("hi");
     }
+    const refOverlayVisible = ref(false)
+    const onClickMenu =() =>{
+      refOverlayVisible.value = !refOverlayVisible.value
+      console.log(refOverlayVisible.value);
+    }
      return () => (
       <div>
         <Navbar>
           {{
             title:()=>"蓝莓记账",
-            icon:()=><Icon name='menu' class={s.navIcon}/>
+            icon:()=><Icon name='menu' class={s.navIcon} onClick={onClickMenu}/>
           }}
         </Navbar>
         <Center class={s.record_wrapper}>
@@ -23,9 +29,13 @@ export const StartPage = defineComponent({
         </Center>
         <div class={s.button_wrapper}>
           <Button class={s.button} onClick={onClick}>
-          记一笔
+            记一笔
           </Button>
           <FloatButton iconName='add'/>
+          {
+            refOverlayVisible.value && 
+            <Overlay onClose ={()=>refOverlayVisible.value = false}/>
+          }
         </div>
       </div>
      )
