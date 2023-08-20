9b747fb0155c0457ec4cc2b09da330bffd668d67
YongTing <jiangyongting0810@gmail.com>
2023-08-20 13:32:36 +0800

    封装 Form 组件

--- src/components/tag/TagForm.tsx
+++ src/components/tag/TagForm.tsx
@@ -1,8 +1,7 @@
 import { defineComponent, PropType, reactive, toRaw } from 'vue';
-import { MainLayout } from '../../layouts/MainLayout';
 import { Button } from '../../shared/Button';
 import { EmojiSelect } from '../../shared/EmojiSelect';
-import { Icon } from '../../shared/Icon';
+import { Form, FormItem } from '../../shared/Form';
 import { Rules, validate } from '../../shared/validate';
 import s from './Tag.module.scss';
 export const TagForm = defineComponent({
@@ -34,43 +33,14 @@ export const TagForm = defineComponent({
       e.preventDefault()
     }
     return () => (
-      <form class={s.form} onSubmit={onSumit}>
-        <div class={s.formRow}>
-          <label class={s.formLabel}>
-            <span class={s.formItem_name}>标签名</span>
-            <div class={s.formItem_value}>
-              <input v-model={formData.name} class={[s.formItem, s.input, s.error]}></input>
-            </div>
-            <div class={s.formItem_errorHint}>
-              <span>
-                {errors['name']?errors['name'][0] : '　'}
-              </span>
-            </div>
-          </label>
-        </div>
-        <div class={s.formRow}>
-          <label class={s.formLabel}>
-            <span class={s.formItem_name}>
-              符号
-              {formData.sign}
-            </span>
-            <div class={s.formItem_value}>
-              <EmojiSelect v-model={formData.sign} class={[s.formItem,s.emojiList,s.error]}/>
-            </div>
-            <div class={s.formItem_errorHint}>
-              <span>
-                {errors['sign'] ? errors['sign'][0] : '　'}
-              </span>
-            </div>
-          </label>
-        </div>
-        <p class={s.tips}>记账时长按标签即可进行编辑</p>
-        <div class={s.formRow}>
-          <div class={s.formItem_value}>
-            <Button class={[s.formItem, s.button]}>确定</Button>
-          </div>
-        </div>
-      </form>
+      <Form onSubmit={onSumit}>
+        <FormItem 
+          label='标签名' 
+          type='text' 
+          v-model={formData.name} 
+          error={errors['name']?errors['name'][0]:''}/>
+        
+      </Form>
     )
   }
 })
