58f91003be6703cd14fbfc51903640dc72807f36
YongTing <jiangyongting0810@gmail.com>
2023-10-13 16:14:48 +0800

    倒计时 60 秒

--- src/shared/Form.tsx
+++ src/shared/Form.tsx
@@ -37,11 +37,29 @@ export const FormItem = defineComponent({
       type:[String,Number]
     },
     options:Array as PropType<Array<{value:string,text:string}>>,
-    onClick:Function as PropType<()=>void>
+    onClick:Function as PropType<()=>void>,
+    countFrom:{
+      type:Number,
+      default:60
+    }
   },
   emit:["update:modelValue"],
   setup: (props, context) => {
     const refDateVisible = ref(false)
+    const timer = ref<number>()
+    const count = ref<number>(props.countFrom)
+    const isCounting = computed(()=>!!timer.value)
+    const onClickSendValidationCode = ()=>{
+      props.onClick?.()
+      timer.value = setInterval(()=>{
+        count.value -= 1
+        if(count.value === 0){
+          clearInterval(timer.value)
+          timer.value = undefined
+          count.value = props.countFrom
+        }
+      },1000)
+    }
     const content = computed(()=>{
       switch(props.type){
         case 'text':
@@ -88,8 +106,8 @@ export const FormItem = defineComponent({
                 onInput={(e:any) => context.emit('update:modelValue',e.target.value)}
                 class={[s.formItem, s.input,s.validationCodeInput,props.error ? s.error : '']}
                 placeholder={props.placeholder}/>
-              <Button onClick={props.onClick} class={[s.formItem,s.validationCode,s.validationCodeButton]}>
-                发送验证码
+              <Button disabled={isCounting.value} onClick={onClickSendValidationCode} class={[s.formItem,s.validationCode,s.validationCodeButton]}>
+                {isCounting.value ? `${count.value}秒后可重新发送`: "发送验证码"}
               </Button>
             </>
         case undefined:
