ebb8222eb73abbe4e6c2722357eeba61ba08039f
YongTing <jiangyongting0810@gmail.com>
2023-10-18 21:43:58 +0800

    每个路由都做登录检查

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -7,6 +7,7 @@ import { Button } from '../shared/Button';
 import { Form, FormItem } from '../shared/Form';
 import { http } from '../shared/Http';
 import { Icon } from '../shared/Icon';
+import { refreshMe } from '../shared/mePromise';
 import { hasError, validate } from '../shared/validate';
 import s from './SignInPage.module.scss';
 export const SignInPage = defineComponent({
@@ -46,6 +47,7 @@ export const SignInPage = defineComponent({
         // router.push("/")
         const returnTo = route.query.return_to?.toString()
         console.log(route.query.return_to);
+        refreshMe()
         router.push(returnTo || '/')
       }
     }
