9a4af2f04808eb2494432ac774d4885a83b6c342
YongTing <jiangyongting0810@gmail.com>
2023-10-20 10:21:31 +0800

    创建mock模块

--- src/shared/Http.tsx
+++ src/shared/Http.tsx
@@ -1,5 +1,10 @@
-import axios, { AxiosError, AxiosInstance, AxiosRequestConfig } from "axios";
-type JSONValue = string | number | null | boolean | JSONValue[] | {[key:string]:JSONValue}
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
+import { mockSession } from "../mock/mock";
+
+type GetConfig = Omit<AxiosRequestConfig,'params'|'url'|'method'>
+type PostConfig = Omit<AxiosRequestConfig, 'url' | 'data' | 'method'>
+type PatchConfig = Omit<AxiosRequestConfig, 'url' | 'data'>
+type deleteConfig = Omit<AxiosRequestConfig, 'params'>
 
 export class Http{
   instance:AxiosInstance
@@ -8,19 +13,44 @@ export class Http{
       baseURL
     })
   }
-  get<R = unknown>(url:string,query?:Record<string,string>,config?:Omit<AxiosRequestConfig,'params'|'url'|'method'>){
+  get<R = unknown>(url:string,query?:Record<string,string>,config?:GetConfig){
     return this.instance.request<R>({...config,url,params:query,method:'get'})
   }
-  post<R = unknown>(url: string, data?: Record<string, JSONValue>, config?: Omit<AxiosRequestConfig, 'url' | 'data' | 'method'>){
+  post<R = unknown>(url: string, data?: Record<string, JSONValue>, config?: PostConfig){
     return this.instance.request<R>({...config,url,data,method:'post'})
   }
-  patch<R = unknown>(url: string, data?: Record<string, JSONValue>, config?: Omit<AxiosRequestConfig, 'url' | 'data'>) {
+  patch<R = unknown>(url: string, data?: Record<string, JSONValue>, config?: PatchConfig) {
     return this.instance.request<R>({ ...config, url, data, method: 'patch' })
   }
   // destroy
-  delete<R = unknown>(url: string, query?: Record<string, string>, config?: Omit<AxiosRequestConfig, 'params'>) {
+  delete<R = unknown>(url: string, query?: Record<string, string>, config?: deleteConfig) {
     return this.instance.request<R>({ ...config, url: url, params: query, method: 'delete' })
   }
+}
+
+const mock = (response:AxiosResponse) => {
+  if(location.hostname !== 'localhost' 
+    &&location.hostname !== '127.0.0.1'
+    &&location.hostname !== '192.168.3.57'
+  ){return false}
+  //查看请求参数里面是不是有_mock的参数
+  switch (response.config?.params?._mock){
+    // case 'tagIndex':
+    //   [response.status, response.data] = mockTagIndex(response.config)
+    //   return true
+    // case 'itemCreate':
+    //   [response.status, response.data] = mockItemCreate(response.config)
+    //   return true
+    // case 'itemIndex':
+    //   [response.status, response.data] = mockItemIndex(response.config)
+    //   return true
+    // case 'tagCreate':
+    //   [response.status, response.data] = mockTagCreate(response.config)
+    case 'session':
+      [response.status, response.data] = mockSession(response.config)
+      return true
+  }
+  return false
 }
 
 export const http = new Http('/api/v1')
@@ -32,6 +62,18 @@ http.instance.interceptors.request.use(config => {
   }
   return config
 })
+
+http.instance.interceptors.response.use((response) => {
+  mock(response)
+  return response
+},(error) =>{
+  if(mock(error.response)){
+    return error.response
+  } else {
+    throw error
+  }
+}
+)
 
 http.instance.interceptors.response.use(response=>{
   return response
