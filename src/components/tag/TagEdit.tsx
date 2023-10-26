819eb467339b267458a2f02ed44cb52de2a99937
YongTing <jiangyongting0810@gmail.com>
2023-10-26 15:34:02 +0800

    可以编辑标签

--- src/components/tag/TagEdit.tsx
+++ src/components/tag/TagEdit.tsx
@@ -1,39 +1,16 @@
-import { defineComponent, PropType, reactive, toRaw } from 'vue';
+import { defineComponent, onMounted } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 import { MainLayout } from '../../layouts/MainLayout';
 import { BackIcon } from '../../shared/BackIcon';
 import { Button } from '../../shared/Button';
-import { EmojiSelect } from '../../shared/EmojiSelect';
-import { Icon } from '../../shared/Icon';
-import { Rules, validate } from '../../shared/validate';
 import s from './Tag.module.scss';
 import { TagForm } from './TagForm';
 export const TagEdit = defineComponent({
-  props: {
-    name: {
-      type: String as PropType<string>
-    }
-  },
   setup: (props, context) => {
-    const formData = reactive({
-      name:'',
-      sign:'',
-    })
-    const errors = reactive<{[k in keyof typeof formData]?:string[]}>({})
-    const onSumit = (e:Event) =>{
-      console.log(toRaw(formData))
-      const rules:Rules<typeof formData> = [
-        { key: 'name', type: 'required', message: '必填' },
-        { key: 'name', type: 'pattern', regex: /^.{1,4}$/, message: '只能填 1 到 4 个字符' },
-        { key: 'sign', type: 'required', message: '必填' },
-      ]
-      console.log(toRaw(errors));
-      Object.assign(errors, {
-        name: undefined,
-        sign: undefined
-      })
-      Object.assign(errors, validate(formData, rules))
-      console.log(toRaw(errors));
-      e.preventDefault()
+    const route = useRoute()
+    const numberId = parseInt(route.params.id!.toString())
+    if(Number.isNaN(numberId)){
+      return ()=><div>id不存在</div>
     }
     return () => (
       <MainLayout>
@@ -41,7 +18,7 @@ export const TagEdit = defineComponent({
           title: ()=>'编辑标签',
           icon:()=><BackIcon/>,
           default:()=><>
-            <TagForm/>
+            <TagForm id={numberId}/>
             <div class={s.actions}>
               <Button level='danger' class={s.removeTags}>删除标签</Button>
               <Button level='danger' class={s.removeTagsAndItems}>删除标签和记账</Button>
--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -46,6 +46,18 @@ export const mockTagIndex: Mock = (config) => {
   } 
 }
 
+export const mockTagShow: Mock = config =>{
+  const createTag = (attrs?: any) =>
+    ({
+      id: createId(),
+      name: faker.lorem.word(),
+      sign: faker.internet.emoji(),
+      kind: 'expenses',
+      ...attrs
+    })
+  return [200,{resource:createTag()}]
+}
+
 export const mockItemCreate: Mock = config => {
   return [422,{
     errors:{
